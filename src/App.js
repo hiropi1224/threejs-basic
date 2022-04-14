478a32226a6d0510217ca22c15014d830998cf3e
Shirahamah <hiropi1224s@gmail.com>
2022-04-14 22:56:13 +0900

    💄 Boxの表示を修正

--- src/App.js
+++ src/App.js
@@ -1,37 +1,26 @@
-import { useRef, useState } from "react";
+import { useRef } from "react";
 import { Canvas, useFrame } from "@react-three/fiber";
 
-function Box(props) {
-  // This reference gives us direct access to the THREE.Mesh object
+const Box = () => {
   const ref = useRef();
-  // Hold state for hovered and clicked events
-  const [hovered, hover] = useState(false);
-  const [clicked, click] = useState(false);
-  // Subscribe this component to the render-loop, rotate the mesh every frame
-  useFrame((state, delta) => (ref.current.rotation.x += 0.01));
-  // Return the view, these are regular Threejs elements expressed in JSX
+  useFrame((state) => {
+    ref.current.rotation.x += 0.01;
+    ref.current.rotation.y += 0.01;
+  });
   return (
-    <mesh
-      {...props}
-      ref={ref}
-      scale={clicked ? 1.5 : 1}
-      onClick={(event) => click(!clicked)}
-      onPointerOver={(event) => hover(true)}
-      onPointerOut={(event) => hover(false)}
-    >
-      <boxGeometry />
-      <meshStandardMaterial color={hovered ? "hotpink" : "orange"} />
+    <mesh ref={ref}>
+      <boxBufferGeometry />
+      <meshBasicMaterial color='blue' />
     </mesh>
   );
-}
+};
 
 export default function App() {
   return (
-    <Canvas>
-      <ambientLight intensity={0.5} />
-      <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} />
-      <pointLight position={[-10, -10, -10]} />
-      <Box />
-    </Canvas>
+    <div style={{ height: "100vh" }}>
+      <Canvas style={{ background: "black" }}>
+        <Box />
+      </Canvas>
+    </div>
   );
 }
